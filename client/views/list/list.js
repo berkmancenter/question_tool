1c21a22069ec5d058cad8189f5551150bf50e344
Nick Rubin <nrubin999@gmail.com>
2015-06-16 10:52:18 -0400

    Updated question list view

--- client/views/list/list.js
+++ client/views/list/list.js
@@ -5,7 +5,8 @@ if (Meteor.isClient) {
 			return Cookie.get("tablename");
 		},
 		description: "Here's a test description",
-		admin: false
+		admin: false,
+		popular: [{indexOne: true, votes: 6, f_time: "11:43 am EDT, 22 May", posterGreaterThanZero: true, shade: "c6", age_marker: "stale", notAdmin: true, emailGreaterThanZero: true, email: "nrubin999@gmail.com", poster: "Nick Rubin", text: "What's up my man?"}]
 	});
 
 }
