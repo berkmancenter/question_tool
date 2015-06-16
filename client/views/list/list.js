ea18c1fcbe456d2786efdd574d1db950f23b3ee1
Nick Rubin <nrubin999@gmail.com>
2015-06-16 14:37:20 -0400

    Random updates

--- client/views/list/list.js
+++ client/views/list/list.js
@@ -1,12 +1,17 @@
-if (Meteor.isClient) {
-
+if (Meteor.isClient) { 
+	
 	Template.list.helpers({
 		tablename: function() {
 			return Cookie.get("tablename");
 		},
 		description: "Here's a test description",
 		admin: false,
-		popular: [{indexOne: true, votes: 6, f_time: "11:43 am EDT, 22 May", posterGreaterThanZero: true, shade: "c6", age_marker: "stale", notAdmin: true, emailGreaterThanZero: true, email: "nrubin999@gmail.com", poster: "Nick Rubin", text: "What's up my man?"}]
+		indexOne: true,
+		emailGreaterThanZero: true,
+		posterGreaterThanZero:true,
+		popular: function() {
+			return Questions.find({ tablename: Cookie.get("tablename") });
+		}
 	});
-
+	
 }
