2690277b685e431375a55aa3e8a8ea473d0485c7
Nick Rubin <nrubin999@gmail.com>
2015-06-18 09:55:03 -0400

    Refactored list.html

--- client/views/list/list.js
+++ client/views/list/list.js
@@ -6,58 +6,9 @@ Template.list.helpers({
 		var table = Instances.findOne({ tablename: Cookie.get("tablename")});
 		return table.description;
 	},
-	popular: function() {
+	question: function() {
 		var table = Instances.findOne({ tablename: Cookie.get("tablename")});
 		var threshhold = table.threshhold;
-		Session.set("threshhold", threshhold);
-		var popQuestions = Questions.find({ tablename: Cookie.get("tablename")});
-		popQuestions = popQuestions.fetch();
-		popQuestions.sort(function(a, b) {
-			if(a.votes > b.votes) {
-				return -1;
-			} else if(a.votes < b.votes) {
-				return 1;
-			} else {
-				return 0;
-			}
-		});
-		var voteAverage = 0;
-		var voteArray = [];
-		for(var i = 0; i < popQuestions.length; i++) {
-			voteAverage += popQuestions[i].votes;
-			voteArray.push(popQuestions[i].votes);
-		}
-		var popular = [];
-		for(var p = 0; p < threshhold; p++) {
-			popular.push(new Array());
-			popular[p].admin = false;
-			popular[p].votes = popQuestions[p].votes;
-			popular[p].text = popQuestions[p].text;
-			popular[p].poster = popQuestions[p].poster;
-			popular[p].email = popQuestions[p].email;
-			if(p%2 == 0) {
-				popular[p].indexOne = true;
-				popular[p].indexTwo = false;
-			} else if(p%2 == 1) {
-				popular[p].indexOne = false;
-				popular[p].indexTwo = true;
-			}
-			popular[p].answerlink = "/answer/" + popQuestions[p]._id;
-			var d = new Date(popQuestions[p].lasttouch);
-			popular[p].f_time = d.toTimeString().substring(0,5) + " " + d.toTimeString().substring(19,22) + " " + d.toDateString().substring(4, 10);
-			var avg = (Math.max.apply(Math, voteArray) + Math.min.apply(Math, voteArray)) / 2;
-			var stddev = (Math.max.apply(Math, voteArray) - Math.min.apply(Math, voteArray))/6;
-			stddev += .001; //Avoides dividing by zero
-			popular[p].shade = "c" + Math.round(3+((popQuestions[p].votes - avg) / stddev));
-			popular[p].age_marker = "stale";
-			var answers = Answers.find({ qid: popQuestions[p]._id });
-			if(answers.fetch().length > 0) {
-				popular[p].answer = answers.fetch();
-			}
-		}
-		return popular;
-	},
-	recent: function() {
 		var questions = Questions.find({ tablename: Cookie.get("tablename") }).fetch();
 		var voteAverage = 0;
 		var voteArray = [];
@@ -75,7 +26,7 @@ Template.list.helpers({
 				return 0;
 			}
 		});
-		for(var i = (Session.get("threshhold")); i < questions.length; i++) {
+		for(var i = 0; i < questions.length; i++) {
 			questions[i].admin = false;
 			if(i%2 == 0) {
 				questions[i].indexOne = true;
@@ -110,6 +61,11 @@ Template.list.helpers({
 			var answers = Answers.find({ qid: questions[i]._id });
 			if(answers.fetch().length > 0) {
 				questions[i].answer = answers.fetch();
+			}
+			if(i < threshhold) {
+				questions[i].popular = true;
+			} else {
+				questions[i].popular = false;
 			}
 		}
 		return questions;
