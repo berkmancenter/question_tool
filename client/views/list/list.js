d8b013b964101235b490194d969cbe3c4bd429f8
Nick Rubin <nrubin999@gmail.com>
2015-06-19 13:28:22 -0400

    Fixed time formatting issues

--- client/views/list/list.js
+++ client/views/list/list.js
@@ -67,19 +67,19 @@ Template.list.helpers({
 				var time24 = d.toTimeString().substring(0,5);
 				var tmpArr = time24.split(':'), time12;
 				if(+tmpArr[0] == 12) {
-					time12 = tmpArr[0] + ':' + tmpArr[1] + ' pm';
+					time12 = tmpArr[0] + ':' + tmpArr[1] + 'pm';
 				} else {
 					if(+tmpArr[0] == 00) {
-						time12 = '12:' + tmpArr[1] + ' am';
+						time12 = '12:' + tmpArr[1] + 'am';
 					} else {
 						if(+tmpArr[0] > 12) {
-							time12 = (+tmpArr[0]-12) + ':' + tmpArr[1] + ' pm';
+							time12 = (+tmpArr[0]-12) + ':' + tmpArr[1] + 'pm';
 						} else {
-							time12 = (+tmpArr[0]) + ':' + tmpArr[1] + ' am';
+							time12 = (+tmpArr[0]) + ':' + tmpArr[1] + 'am';
 						}
 					}
 				}
-				questions[i].f_time = time12 + " " + d.toTimeString().substring(19,22) + " " + d.toDateString().substring(4, 10);
+				questions[i].f_time = time12 + " " + d.toDateString().substring(4, 10);
 				var avg = (Math.max.apply(Math, voteArray) + Math.min.apply(Math, voteArray)) / 2;
 				//var stddev = (Math.max.apply(Math, voteArray)-Math.min.apply(Math, voteArray))/6;
 				var stddev = standardDeviation(voteArray);
