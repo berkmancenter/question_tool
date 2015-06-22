d3f0bf6924fb5d6fd38c18fa78b5902348eb6cf3
Nick Rubin <nrubin999@gmail.com>
2015-06-22 10:28:36 -0400

    Added client-side subscriptions file

---
+++ client/subscriptions.js
@@ -0,0 +1,6 @@
+Tracker.autorun(function() {
+	Meteor.subscribe('instances', Cookie.get("tablename"));
+	Meteor.subscribe('instances', Cookie.get("tablename"));
+	Meteor.subscribe('instances', Cookie.get("tablename"));
+	Meteor.subscribe('instances', Cookie.get("tablename"));
+});
