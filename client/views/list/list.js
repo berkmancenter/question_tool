ede2e92b043ab5f09cf2282b393c7647227a4e4e
Nick Rubin <nrubin999@gmail.com>
2015-06-24 13:53:37 -0400

    Added interact.js for dragging questions

--- client/views/list/list.js
+++ client/views/list/list.js
@@ -25,6 +25,38 @@ Template.list.onCreated(function () {
 
 Template.list.onRendered(function() {
 	document.title = "Live Question Tool";
+	/*interact('.question')
+	.draggable({
+		inertia: true,
+		restrict: {
+			restriction: "parent",
+			endOnly: true,
+			elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+		},
+
+		onmove: dragMoveListener,
+		onend: function (event) {
+			var textEl = event.target.querySelector('p');
+
+			textEl && (textEl.textContent =
+				'moved a distance of '
+				+ (Math.sqrt(event.dx * event.dx +
+					event.dy * event.dy)|0) + 'px');
+				}
+			});
+
+			function dragMoveListener (event) {
+				var target = event.target,
+				x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+				y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+				target.style.webkitTransform =
+				target.style.transform =
+				'translate(' + x + 'px, ' + y + 'px)';
+
+				target.setAttribute('data-x', x);
+				target.setAttribute('data-y', y);
+			}*/
 });
 
 Template.list.helpers({
