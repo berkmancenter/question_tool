85bc0f3603e9468b0fa34118fcc5d97b56bf14ac
Nick Rubin <nrubin999@gmail.com>
2015-06-24 14:49:02 -0400

    Added combine view

---
+++ client/views/combine/combine.js
@@ -0,0 +1,35 @@
+Template.modify.onCreated(function () {
+	Meteor.call('cookieCheck', Cookie.get("tablename"), function (error, result) {
+		if(!result) {
+			window.location.href = "/";
+		} else {
+			Meteor.call('adminCheck', Cookie.get("admin_pw"), Cookie.get("tablename"), function (error, result) {
+				if(!result) {
+					window.location.href = "/list";
+				}
+			});
+		}
+	});
+});
+
+Template.modify.onRendered(function() {
+	document.title = "Live Question Tool Modification Form";
+});
+
+Template.modify.events({
+	"click #submitbutton": function(event, template) {
+		var question = document.getElementsByName("comment")[0].value;
+		Meteor.call('modify', question, template.data._id, Cookie.get("admin_pw"), Cookie.get("tablename"), function (error, result) { 
+			if(!result) {
+				window.location.href = "/list";
+			}
+		});
+	},
+	"keypress #modifybox": function(e, template) {
+		e.which = e.which || e.keyCode;
+		if(e.which == 13) {
+			e.preventDefault();
+			document.getElementById("submitbutton").click();
+		}
+	}
+});
