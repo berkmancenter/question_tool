ad571b329e4ac32b25012ac298eb4e4fec998b6d
Nick Rubin <nrubin999@gmail.com>
2015-06-24 10:34:40 -0400

    Finished 'create' page input validation

--- client/views/create/create.js
+++ client/views/create/create.js
@@ -11,11 +11,22 @@ Template.create.events({
 		var stale = staleSelect[staleSelect.selectedIndex].value;
 		var description = document.getElementsByName("description")[0].value;
 		Meteor.call('create', tablename, threshhold, redLength, stale, description, passwordConfirm, function (error, result) {
-			console.log(result);
 			if(typeof result === 'object') {
 				var errorString = "";
 				for(var e = 0; e < result.length; e++) {
-					errorString += "Error #" + (e + 1) + " : " + result[e].name + "\n";
+					if(result[e].name == "tablename") {
+						errorString += "Error #" + (e + 1) + " : Please enter a valid table name using only letters and numbers.\n\n";
+					} else if(result[e].name == "threshhold") {
+						errorString += "Error #" + (e + 1) + " : Please enter a valid # of 'featured' questions using the drop down menu.\n\n";
+					} else if(result[e].name == "new_length") {
+						errorString += "Error #" + (e + 1) + " : Please enter a valid value using the 'new questions' drop down menu.\n\n";
+					} else if(result[e].name == "stale_length") {
+						errorString += "Error #" + (e + 1) + " : Please enter a valid value using the 'old questions' drop down menu.\n\n";
+					} else if(result[e].name == "description") {
+						errorString += "Error #" + (e + 1) + " : Please enter a valid description under 255 characters.\n\n";
+					} else if(result[e].name == "password") {
+						errorString += "Error #" + (e + 1) + " : Please enter a valid password using letters and numbers and between 4 and 10 characters.\n\n";
+					}
 				}
 				alert(errorString);
 			} else {
