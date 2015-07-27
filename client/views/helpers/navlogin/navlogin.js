061dc4b5dc4fe81c5c24038062cf5eb2fcb707ed
Nick Rubin <nrubin999@gmail.com>
2015-07-27 11:40:45 -0400

    Moved login/register to popups instead of separate pages

--- client/views/helpers/navlogin/navlogin.js
+++ client/views/helpers/navlogin/navlogin.js
@@ -4,12 +4,80 @@ Template.userInfo.events({
 		window.location.reload();
 	},
 	"click #navLogin": function(event, template) {
-		var URL = window.location.href;
+		/*var URL = window.location.href;
 		var split = URL.split("/");
 		var page = split[split.length-1];
-		Router.go('/login/' + page);
+		Router.go('/login/' + page);*/
+		var parentNode = document.getElementById("banner");
+		popoverTemplate = Blaze.render(Template.login, parentNode);
 	},
 	"click #navRegister": function(event, template) {
-		Router.go('/register');
+		var parentNode = document.getElementById("banner");
+		popoverTemplate = Blaze.render(Template.register, parentNode);
+	},
+	"click #darker": function(event, template) {
+		$(".formcontainer").fadeOut(400);
+		$("#darker").fadeOut(400, function() {
+			Blaze.remove(popoverTemplate);
+		});
+	},
+	"click #loginsubmitbutton": function(event, template) {
+		var email = document.getElementById("loginemail").value;
+		var password = document.getElementById("passwordbox").value;
+		if(!email) {
+			alert("Please enter a valid email address.");
+			return false;
+		} else if(!password) {
+			alert("Please enter a valid password.");
+			return false;
+		}
+		Meteor.loginWithPassword(email, password, function(error) {
+			if(!error) {
+				/*if(template.data) {
+					window.location.href = "/" + template.data;
+				} else {
+					window.location.href = "/";
+				}*/
+				$(".formcontainer").fadeOut(400);
+				$("#darker").fadeOut(400, function() {
+					Blaze.remove(popoverTemplate);
+				});
+			} else {
+				alert(error);
+			}
+		})
+	},
+	"click #registersubmitbutton": function(event, template) {
+		var email = document.getElementById("loginemail").value;
+		var loginName = document.getElementById("loginname").value;
+		var password1 = document.getElementById("passwordbox").value;
+		var password2 = document.getElementById("passwordconfirm").value;
+		if(!email) {
+			alert("The email field cannot be left blank. Please try again.");
+			return false;
+		} else if (!loginName) {
+			alert("The name field cannot be left blank. Please try again.");
+			return false;
+		} else if (password1 != password2) {
+			alert("Passwords do not match. Please try again.");
+			return false;
+		}
+		Accounts.createUser({
+			email: email,
+			password: password2,
+			profile: {
+				name: loginName
+			}
+		}, function(error) {
+			if(error) {
+				alert(error);
+				return false;
+			} else {
+				$(".formcontainer").fadeOut(400);
+				$("#darker").fadeOut(400, function() {
+					Blaze.remove(popoverTemplate);
+				});
+			}
+		})
 	}
 });
--- client/views/login/login.js
+++ client/views/login/login.js
@@ -1,10 +1,10 @@
-Template.register.onRendered(function() {
+Template.login.onRendered(function() {
 	$(".formcontainer").hide().fadeIn(400);
 	$("#darker").hide().fadeIn(400);
 });
 
-Template.register.events({
-	"keypress #passwordconfirm": function(event, template) {
+Template.login.events({
+	"keypress #passwordbox": function(event, template) {
 		event.which = event.which || event.keyCode;
 		if(event.which == 13) {
 			event.preventDefault();
