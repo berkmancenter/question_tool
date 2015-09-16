a56aeca683fdceedc14aedce5259c17a6d5d6bfa
Matthew Zagaja <mzagaja@alum.wpi.edu>
2015-09-16 14:38:12 -0400

    Prevent creation of instances with same tablename.

--- server/methods.js
+++ server/methods.js
@@ -107,6 +107,11 @@ Meteor.methods({
 		if(!Meteor.user()) {
 			return false;
 		}
+
+		if(Instances.find({ tablename: tablename }).count() > 0) {
+			return false;
+		}
+
 		var keys;
 		if(mods.length > 4) {
 			var errors = [
