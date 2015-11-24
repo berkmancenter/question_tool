3462a03a8ff306d97858872b1ecb0e500d4a8179
Matthew Zagaja <mzagaja@alum.wpi.edu>
2015-11-24 13:22:15 -0500

    Update credits to be more responsive.

--- client/views/footer/footer.js
+++ client/views/footer/footer.js
@@ -2,11 +2,5 @@ Template.footer.events({
 	"click #creditsbutton": function(event, template) {
 		var parentNode = document.getElementById("banner");
 		popoverTemplate = Blaze.render(Template.credits, parentNode);
-	},
-	"click #darker": function(event, template) {
-		$(".formcontainer").fadeOut(400);
-		$("#darker").fadeOut(400, function() {
-			Blaze.remove(popoverTemplate);
-		});
 	}
 });
