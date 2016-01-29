dd6ae7679f0f18c7cb9a8f27da4aef330eaf5242
Matthew Zagaja <mzagaja@alum.wpi.edu>
2016-01-29 16:00:12 -0500

    Email users when their instance is going to expire.
    This commit adds syncedcron which will run background tasks.
    It also adds a background task that checks for old instances
    and e-mails the administrators to warn them their instance
    will expire. Plus a route to prevent that from occuring.

---
+++ server/jobs.js
@@ -0,0 +1,20 @@
+SyncedCron.add({
+  name: 'EMail Administrator when 30 Day Deletion is Approaching',
+  schedule: function(parser) {
+    // parser is a later.parse object
+    return parser.text('every 1 minute');
+  },
+  job: function() {
+    var today = new Date();
+    var thirtyDaysAgo = new Date(today.getTime() - 30*24*60*60*1000);
+    var oldInstances = Instances.find({ lasttouch: { $lt: +thirtyDaysAgo } } ).fetch();
+    for(var i = 0; i < oldInstances.length; i++) {
+    	Email.send({
+    		to: oldInstances[i].admin,
+    		from: Meteor.users.findOne().emails[0].address,
+    		subject: 'Action Required: Your Question Tool Instance is Going to Expire Soon',
+    		text: 'Your instance on the Question Tool is about to expire. You can prevent this from happening by clicking the following link ' + Meteor.absoluteUrl() + 'list/' + oldInstances[i].slug + '/touch'
+    	});
+    }
+  }
+});
