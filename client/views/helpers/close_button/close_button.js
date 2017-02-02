a2a4f2006b3a5d9f07ebe493af286736015df8c2
Mariam Maarouf <mrf.mariam@gmail.com>
2017-02-02 20:52:30 +0200

    Esc button to exit popovers

--- client/views/helpers/close_button/close_button.js
+++ client/views/helpers/close_button/close_button.js
@@ -1,10 +1,22 @@
 /* eslint-disable func-names, no-unused-vars */
+function removePopover() {
+  $('.formcontainer').fadeOut(400);
+  $('#darker').fadeOut(400, () => {
+    Blaze.remove(popoverTemplate);
+  });
+}
+
+Template.close_button.onCreated(function () {
+  $(document).keydown((e) => {
+    if (popoverTemplate && e.keyCode === 27) {
+      removePopover();
+    }
+  });
+});
+
 Template.close_button.events({
   'click .closecontainer': function (event, template) {
-    $('.formcontainer').fadeOut(400);
-    $('#darker').fadeOut(400, () => {
-      Blaze.remove(popoverTemplate);
-    });
+    removePopover();
   },
 });
 /* eslint-disable func-names, no-unused-vars */
