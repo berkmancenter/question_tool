39e02d636af03190d855072020a5363505b4317f
Salil-Jain <j.salil36@gmail.com>
2018-07-26 01:21:17 +0530

    changed mail_url back to localhost

--- server/lib/startup.js
+++ server/lib/startup.js
@@ -2,7 +2,7 @@ Meteor.startup(() => {
   // Email of Question Tool super admin. Has control over Instances, etc.
   process.env.SUPERADMIN_EMAIL = 'questiontool@admin.com';
   // URL of mail server goes here for email sending
-  process.env.MAIL_URL = 'smtp://user:password@mailhost:port/'
+  process.env.MAIL_URL = 'http://localhost:3000/';
 
   SyncedCron.start();
 });
