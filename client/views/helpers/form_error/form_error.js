4410fe45fd62c5e15c601845a65cbb646e6f343e
Avinash Agarwal <agarwalavinash14@gmail.com>
2019-02-03 00:54:52 +0530

    Flash-message

--- client/views/helpers/form_error/form_error.js
+++ client/views/helpers/form_error/form_error.js
@@ -1,5 +1,7 @@
 Template.form_error.onRendered(() => {
   $('.error').hide().fadeIn(400);
+  setTimeout(function(){ $('.error').fadeOut(400); }, 5000);
+  
 });
 
 /* eslint-disable func-names, no-unused-vars */
