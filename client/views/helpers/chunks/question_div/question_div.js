a94735628dda5926363bf80fd1059a1e390dea24
singhsanket143 <singhsanket143@gmail.com>
2019-06-01 22:14:38 +0530

    Updated the display span of no. of votes for a question of an instance

--- client/views/helpers/chunks/question_div/question_div.js
+++ client/views/helpers/chunks/question_div/question_div.js
@@ -1,12 +1,16 @@
-import { Votes, Answers, Instances, Questions } from '/lib/common.js';
+import { Votes, Answers, Instances, Questions } from "/lib/common.js";
 
-Template.question_div.onCreated(function () {
+Template.question_div.onCreated(function() {
   this.replyCount = new ReactiveVar(0);
 });
 
 Template.question_div.helpers({
   isPoster() {
-    return Meteor.user() && Meteor.user().emails[0].address === this.email && this.posterLoggedIn;
+    return (
+      Meteor.user() &&
+      Meteor.user().emails[0].address === this.email &&
+      this.posterLoggedIn
+    );
   },
 
   replyCount() {
@@ -18,7 +22,7 @@ Template.question_div.helpers({
   },
 
   isDisabled() {
-    if (Questions.findOne({ _id: this._id }).state == 'disabled') {
+    if (Questions.findOne({ _id: this._id }).state == "disabled") {
       return true;
     }
     return false;
@@ -30,54 +34,54 @@ Template.question_div.helpers({
 
   hasAnswers() {
     const answers = Answers.find({
-      qid: this._id,
+      qid: this._id
     });
     return answers.fetch().length > 0;
   },
 
   answersCount() {
     const count = Answers.find({ qid: this._id }).fetch().length;
-    const base = 'repl';
-    const add = count > 1 ? 'ies' : 'y';
-    return count + ' ' + base + add;
+    const base = "repl";
+    const add = count > 1 ? "ies" : "y";
+    return count + " " + base + add;
   },
 
   voteCount() {
-    return Votes.find({ qid: this._id }).fetch().length;
+    return Questions.findOne({ _id: this._id }).votes; // Query to fetch the number of upvotes done on any Question for an Instance
   },
 
   date_format(timeorder) {
-    return moment(timeorder).format('LLL');
+    return moment(timeorder).format("LLL");
   },
 
   time_format(timeorder) {
     return moment(timeorder).fromNow();
-  },
+  }
 });
 
 Template.question_div.events({
-  'click .replybutton': function (event, template) {
+  "click .replybutton": function(event, template) {
     template.replyCount.set(0);
-    $('.replybottom').slideUp();
-    $('.replyarea').val('');
-    $('.replybutton').html('Reply');
+    $(".replybottom").slideUp();
+    $(".replyarea").val("");
+    $(".replybutton").html("Reply");
     const theID = event.target.id.substring(5);
-    const theArea = document.getElementById('down' + theID);
-    if (theArea.style.display === 'none' || !theArea.style.display) {
-      document.getElementById('reply' + theID).innerHTML = 'Close';
-      $('#down' + theID).slideDown(400, function () {
-        $(this).css('display', 'block');
+    const theArea = document.getElementById("down" + theID);
+    if (theArea.style.display === "none" || !theArea.style.display) {
+      document.getElementById("reply" + theID).innerHTML = "Close";
+      $("#down" + theID).slideDown(400, function() {
+        $(this).css("display", "block");
       });
-      $('#text' + theID).focus();
+      $("#text" + theID).focus();
     } else {
-      if (typeof replyError !== 'undefined') {
+      if (typeof replyError !== "undefined") {
         Blaze.remove(replyError);
       }
-      document.getElementById('reply' + theID).innerHTML = 'Reply';
-      $('#down' + theID).slideUp();
+      document.getElementById("reply" + theID).innerHTML = "Reply";
+      $("#down" + theID).slideUp();
     }
   },
-  'keyup .replyarea': function (event, template) {
+  "keyup .replyarea": function(event, template) {
     const urlRegex = /(https?:\/\/(?:www\.|(?!www))[^\s\.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,})/g;
     const found = event.target.value.match(urlRegex);
     let total = 0;
@@ -86,19 +90,34 @@ Template.question_div.events({
       for (let f = 0; f < found.length; f++) {
         totalURL += found[f].length;
       }
-      total = (event.target.value.length - totalURL) + found.length;
-      $(event.target).attr('maxlength', Number(Instances.findOne({ _id: template.data.instanceid }).max_response + totalURL - found.length));
+      total = event.target.value.length - totalURL + found.length;
+      $(event.target).attr(
+        "maxlength",
+        Number(
+          Instances.findOne({ _id: template.data.instanceid }).max_response +
+            totalURL -
+            found.length
+        )
+      );
     } else {
       total = event.target.value.length;
     }
     template.replyCount.set(total);
   },
-  'click .showreplies': function (event, template) {
-    const parentNode = document.getElementById('main-wrapper');
-    popoverTemplate = Blaze.renderWithData(Template.answers, template.data._id, parentNode);
+  "click .showreplies": function(event, template) {
+    const parentNode = document.getElementById("main-wrapper");
+    popoverTemplate = Blaze.renderWithData(
+      Template.answers,
+      template.data._id,
+      parentNode
+    );
   },
-  'click .adminquestionmodify': function (event, template) {
-    const parentNode = document.getElementById('nav');
-    popoverTemplate = Blaze.renderWithData(Template.modify, template.data._id, parentNode);
-  },
+  "click .adminquestionmodify": function(event, template) {
+    const parentNode = document.getElementById("nav");
+    popoverTemplate = Blaze.renderWithData(
+      Template.modify,
+      template.data._id,
+      parentNode
+    );
+  }
 });
