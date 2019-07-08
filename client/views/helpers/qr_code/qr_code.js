c54ac03286776a3f06f81069e427e16152674afb
singhsanket143 <singhsanket143@gmail.com>
2019-07-08 19:59:36 +0530

    Added template helpers and updated styles

--- client/views/helpers/qr_code/qr_code.js
+++ client/views/helpers/qr_code/qr_code.js
@@ -1,4 +1,10 @@
-Template.qrcode.onRendered(() => {
+Template.qr_code.helpers({
+  link() {
+    return 'https://www.gmail.com'
+  }
+})
+
+Template.qr_code.onRendered(() => {
   $('.formcontainer').hide().fadeIn(400);
   $('#darker').hide().fadeIn(400);
 });
