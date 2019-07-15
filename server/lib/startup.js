c46350d6b7d7591ec78475d2ce686d99feacb7fb
singhsanket143 <singhsanket143@gmail.com>
2019-07-15 22:45:40 +0530

    Removed the env variable of SMTP

--- server/lib/startup.js
+++ server/lib/startup.js
@@ -1,8 +1,6 @@
 Meteor.startup(() => {
   // Email of Question Tool super admin. Has control over Instances, etc.
   process.env.SUPERADMIN_EMAIL = 'questiontool@admin.com';
-  // URL of mail server goes here for email sending
-  process.env.MAIL_URL = 'http://localhost:3000/';
 
   SyncedCron.start();
 
