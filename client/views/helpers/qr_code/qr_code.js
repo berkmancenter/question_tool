337cedd0a5e68bd3371158e75e883ac6cd7d0cb9
singhsanket143 <singhsanket143@gmail.com>
2019-07-30 00:51:11 +0530

    Fix: Updated the code style of QrCode template

--- client/views/helpers/qr_code/qr_code.js
+++ client/views/helpers/qr_code/qr_code.js
@@ -2,9 +2,9 @@ Template.qr_code.helpers({
   link() {
     return Template.instance().data.link;
   }
-})
+});
 
-Template.qr_code.onRendered(() => {
+Template.qrcode.onRendered(() => {
   $('.formcontainer').hide().fadeIn(400);
   $('#darker').hide().fadeIn(400);
 });
